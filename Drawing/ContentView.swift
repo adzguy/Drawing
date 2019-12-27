71b556bacd02c5345d34df947624424c43b1cee7
Davron <devrannur@gmail.comn>
2019-12-27 15:12:51 -0600

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Davron on 12/27/19.
+//  Copyright © 2019 Davron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
