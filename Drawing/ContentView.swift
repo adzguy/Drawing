baadcf886acc362a3b8342ee2ec9a1d81b2e77eb
Davron <devrannur@gmail.comn>
2019-12-29 03:19:41 -0600

    AnimatablePair use to animate more/complex shapes.

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,38 +8,54 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: CGFloat
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
     
-    var animatableData: CGFloat {
-        get { insetAmount }
-        set { insetAmount = newValue }
+    public var animatableData: AnimatablePair<Double, Double> {
+        get { AnimatablePair(Double(rows), Double(columns)) }
+        set { self.rows = Int(newValue.first); self.columns = Int(newValue.second)}
     }
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
-        
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
         return path
     }
-    
 }
 struct ContentView: View {
-    @State private var insetAmount: CGFloat = 50
+    @State private var rows = 4
+    @State private var columns = 4
+    
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 300, height: 300)
+        Checkerboard(rows: rows, columns: columns)
             .onTapGesture {
-                withAnimation {
-                  self.insetAmount = CGFloat.random(in: 10...120)
+                withAnimation(.linear(duration: 2)) {
+                    self.rows = 12
+                    self.columns = 12
                 }
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
