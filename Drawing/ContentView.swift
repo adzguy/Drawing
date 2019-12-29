c3e8f79e73e981eda3fd0e47a364a5393ad03280
Davron <devrannur@gmail.comn>
2019-12-29 00:54:03 -0600

    ImagePaint update

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -11,9 +11,17 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Capsule()
-            .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
-        .frame(width: 300, height: 200)
+        VStack {
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
+            
+            Text("Hello You")
+                .frame(width: 300, height: 300)
+                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.5, width: 1, height: 0.5), scale: 0.2), width: 20)
+                
+        }
+
     }
 }
 struct ContentView_Previews: PreviewProvider {
